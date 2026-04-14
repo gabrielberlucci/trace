2efabba53b0ef3dd6c888b74268757e5d3bb94e2
gabriel.berlucci <gabrielberlucci@proton.me>
2026-04-14 19:21:28 -0300

    feat: add a new global error handler

---
+++ app/server/src/middleware/general.error.middleware.ts
@@ -0,0 +1,31 @@
+import { NotFound } from '@/error/NotFound';
+import { UniqueConstraint } from '@/error/UniqueConstraint';
+import type { NextFunction, Request, Response } from 'express';
+import { ReasonPhrases, StatusCodes } from 'http-status-codes';
+
+export const validateError = (
+  error: Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+) => {
+  if (error instanceof NotFound) {
+    return res.status(error.statusCode).send({
+      error: ReasonPhrases.NOT_FOUND,
+      errorName: error.name,
+      message: error.message,
+    });
+  }
+
+  if (error instanceof UniqueConstraint) {
+    return res.status(error.statusCode).send({
+      error: ReasonPhrases.CONFLICT,
+      errorName: error.name,
+      message: error.message,
+    });
+  }
+
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+    error: ReasonPhrases.INTERNAL_SERVER_ERROR,
+  });
+};
