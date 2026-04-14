a04e7690e29b9f67bc5dfc2c54193d8f83da2028
gabriel.berlucci <gabrielberlucci@proton.me>
2026-04-14 19:10:52 -0300

    feat: add use for globalerrormiddleware

--- app/server/src/app.ts
+++ app/server/src/app.ts
@@ -2,6 +2,7 @@ import express, { type Express } from 'express';
 import cors from 'cors';
 import { routes } from './routes';
 import cookieParser from 'cookie-parser';
+import { validateError } from './middleware/general.error.middleware';
 
 const app: Express = express();
 
@@ -9,5 +10,6 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(routes);
+app.use(validateError);
 
 export { app };
