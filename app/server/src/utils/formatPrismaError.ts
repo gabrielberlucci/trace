7dac297e2d019c465605d0b6d6fbe0f214fdd50d
gabriel.berlucci <gabrielberlucci@proton.me>
2026-04-15 00:11:19 -0300

    feat: add an early return if not founded driverAdapterError

--- app/server/src/utils/formatPrismaError.ts
+++ app/server/src/utils/formatPrismaError.ts
@@ -5,10 +5,18 @@ export const formatPrismaError = (
 ) => {
   const jsonError = JSON.parse(JSON.stringify(error.meta));
   const modelName = jsonError.modelName;
-  const prismaErrorMessage = jsonError.driverAdapterError.cause.originalMessage;
+  const driverAdapterError = jsonError?.driverAdapterError;
+
+  if (!driverAdapterError) {
+    return modelName;
+  }
+
+  const prismaErrorMessage =
+    jsonError?.driverAdapterError?.cause?.originalMessage;
 
   const match = prismaErrorMessage?.match(/\"(.*?)\"/);
   const errorMessageFinal = match[1].match(/\_(.*?)\_/);
+
   const firstMessage = errorMessageFinal[1];
 
   return { firstMessage, modelName };
