967a9f533e11ba6517709472eb51115837b458c7
gabriel.berlucci <gabrielberlucci@proton.me>
2026-04-16 03:41:04 -0300

    refactor: move the errors to globalmiddlewarehandlers

--- app/server/src/middleware/general.validation.middleware.ts
+++ app/server/src/middleware/general.validation.middleware.ts
@@ -1,25 +1,9 @@
 import type { Request, Response, NextFunction } from 'express';
-import { z, ZodError } from 'zod';
-import { getReasonPhrase, StatusCodes } from 'http-status-codes';
+import { z } from '../config/zod.config';
 
 export const validateData =
   (schema: z.ZodObject) =>
   (req: Request, res: Response, next: NextFunction) => {
-    try {
-      schema.strict().parse(req.body);
-      next();
-    } catch (error) {
-      if (error instanceof ZodError) {
-        const zodErrorMessage = z.flattenError(error);
-
-        return res.status(StatusCodes.BAD_REQUEST).send({
-          error: getReasonPhrase(StatusCodes.BAD_REQUEST),
-          details: zodErrorMessage.fieldErrors,
-        });
-      } else {
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
-          error: getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR),
-        });
-      }
-    }
+    schema.strict().parse(req.body);
+    next();
   };
