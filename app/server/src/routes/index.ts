c6f7b219a8824f99cab3bcbdb1c0297d47af344a
gabriel.berlucci <gabrielberlucci@proton.me>
2026-04-16 03:39:55 -0300

    feat: add products route

--- app/server/src/routes/index.ts
+++ app/server/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { customerRoute } from './customer.route';
 import { supplierRoute } from './supplier.route';
+import { productRouter } from './product.route';
 
 const routes: Router = Router();
 
 routes.use('/api/customer', customerRoute);
 routes.use('/api/supplier', supplierRoute);
+routes.use('/api/product', productRouter);
 
 export { routes };
