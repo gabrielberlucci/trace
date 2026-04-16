c3c76164f0bc0f999c505c96a7b7aa309789e306
gabriel.berlucci <gabrielberlucci@proton.me>
2026-04-16 03:40:13 -0300

    feat: bring zod vaalidation to globalmiddleware

--- app/server/src/middleware/general.error.middleware.ts
+++ app/server/src/middleware/general.error.middleware.ts
@@ -1,9 +1,9 @@
-import { BadRequest } from '@/error/index';
+import { BadRequest, UnprocessableEntity } from '@/error/index';
 import { Prisma } from '../../generated/prisma/client';
 import { ReasonPhrases, StatusCodes } from 'http-status-codes';
 import { formatPrismaError } from '@/utils/index';
 import type { NextFunction, Request, Response } from 'express';
-import { UnprocessableEntity } from '@/error/UnprocessableEntity';
+import z, { ZodError } from 'zod';
 
 export const validateError = (
   error: Error,
@@ -11,6 +11,17 @@ export const validateError = (
   res: Response,
   _next: NextFunction,
 ) => {
+  // console.log(error);
+  if (error instanceof ZodError) {
+    const flattenError = z.flattenError(error);
+
+    return res.status(StatusCodes.BAD_REQUEST).send({
+      errorName: ReasonPhrases.BAD_REQUEST,
+      formErrors: flattenError.formErrors,
+      fieldErrors: flattenError.fieldErrors,
+    });
+  }
+
   if (error instanceof BadRequest) {
     return res.status(error.statusCode).send({
       errorName: error.name,
@@ -41,6 +52,7 @@ export const validateError = (
     }
   }
 
+  console.error(error);
   return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
     error: ReasonPhrases.INTERNAL_SERVER_ERROR,
   });
