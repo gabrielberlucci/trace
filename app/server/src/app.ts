1f65f1e10cdf1d6208a4820be61a30c94fd1409b
gabriel.berlucci <gabrielberlucci@proton.me>
2026-04-17 19:37:50 -0300

    feat: add app.use() for logs middleware and pinoHttp

--- app/server/src/app.ts
+++ app/server/src/app.ts
@@ -1,14 +1,19 @@
 import express, { type Express } from 'express';
 import cors from 'cors';
-import { routes } from './routes/index';
+import { routes } from '@/routes/index';
 import cookieParser from 'cookie-parser';
-import { validateError } from './middleware/general.error.middleware';
+import { validateError } from '@/middleware/general.error.middleware';
+import { logger } from '@/logger/pino.logger';
+import { pinoHttp } from 'pino-http';
+import { logsMiddleware } from './middleware/general.logs.middleware';
 
 const app: Express = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+app.use(pinoHttp({ logger }));
+app.use(logsMiddleware);
 app.use(routes);
 app.use(validateError);
 
