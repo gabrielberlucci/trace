f89a4628c74bee351c0c049e54ca58424eb1433a
gabriel.berlucci <gabrielberlucci@proton.me>
2026-04-17 19:40:07 -0300

    feat: add loggerStorage pasasing req.log, being acessible anywere

---
+++ app/server/src/middleware/general.logs.middleware.ts
@@ -0,0 +1,12 @@
+import type { NextFunction, Request, Response } from 'express';
+import { loggerStorage } from '@/logger/storage';
+
+export const logsMiddleware = (
+  req: Request,
+  _res: Response,
+  next: NextFunction,
+) => {
+  loggerStorage.run(req.log, () => {
+    next();
+  });
+};
