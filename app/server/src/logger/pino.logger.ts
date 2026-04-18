902e4221f8498be9a1342b18b44a5a5437fed2e3
gabriel.berlucci <gabrielberlucci@proton.me>
2026-04-17 23:39:12 -0300

    feat: add logs for stack trace

--- app/server/src/logger/pino.logger.ts
+++ app/server/src/logger/pino.logger.ts
@@ -2,6 +2,9 @@ import pino from 'pino';
 
 export const logger = pino({
   level: process.env.LOG_LEVEL,
+  serializers: {
+    err: pino.stdSerializers.err,
+  },
   transport:
     process.env.NODE_ENV === 'development'
       ? {
