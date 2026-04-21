ae81454373517c0c9763ecd43de8bee25a869949
gabriel.berlucci <gabrielberlucci@proton.me>
2026-04-21 03:12:29 -0300

    feat: add commonSchema

---
+++ app/server/src/schemas/index.ts
@@ -0,0 +1,14 @@
+import { createCustomerSchema, modifyCustomerSchema } from './customer.schema';
+import { productSchema, modifyProductSchema } from './product.schema';
+import { createSupplierSchema, modifySupplierSchema } from './supplier.schema';
+import { commonSchema } from './common.schema';
+
+export {
+  createCustomerSchema,
+  modifyCustomerSchema,
+  productSchema,
+  modifyProductSchema,
+  createSupplierSchema,
+  modifySupplierSchema,
+  commonSchema,
+};
