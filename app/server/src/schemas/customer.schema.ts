ad731abfc1c4daf5ff51da9ef2a7ef31080f01af
gabriel.berlucci <gabrielberlucci@proton.me>
2026-04-22 23:58:28 -0300

    feat: add birthdate type

--- app/server/src/schemas/customer.schema.ts
+++ app/server/src/schemas/customer.schema.ts
@@ -1,10 +1,20 @@
 import { commonSchema } from '@/schemas/common.schema';
+import { z } from '@/config/zod.config';
 
-export const createCustomerSchema = commonSchema;
+export const createCustomerSchema = commonSchema.extend({
+  birthdate: z.coerce
+    .date({
+      error: (issue) =>
+        issue.input === undefined ? 'Insira uma data' : 'Data inválida',
+    })
+    .min(new Date('1900-01-01'), { error: 'Cliente muito velho' })
+    .max(new Date(), { error: 'Cliente muito novo' })
+    .optional(),
+});
 /*
  * we don't want that the user can modify CPF/CNPJ
  * it can cause problems
  */
-export const modifyCustomerSchema = commonSchema
+export const modifyCustomerSchema = createCustomerSchema
   .partial()
   .omit({ document: true });
