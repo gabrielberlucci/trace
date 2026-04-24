fd54e2e2cc5507daed111948c14713fb4c51b2b9
gabriel.berlucci <gabrielberlucci@proton.me>
2026-04-24 02:08:34 -0300

    feat: add service for getting paginatedData

--- app/server/src/services/product.service.ts
+++ app/server/src/services/product.service.ts
@@ -1,5 +1,7 @@
 import { prisma } from '../../lib/prisma';
 import { Prisma } from '../../generated/prisma/client';
+import { getPaginatedData } from '@/repositories/paginated.repositorhy';
+import type { ProductQueryParamsFilters } from '@/types';
 
 export const createProduct = async (productData: Prisma.ProductCreateInput) => {
   const product = await prisma.product.create({ data: productData });
@@ -20,4 +22,31 @@ export const modifyProduct = async (
   });
 
   return modifiedProduct;
+};
+
+export const getPaginatedProducts = async (
+  queryFilters: ProductQueryParamsFilters,
+) => {
+  const where: Prisma.ProductWhereInput = {
+    ...(queryFilters.barcode && { barcode: queryFilters.barcode }),
+    ...(queryFilters.description && {
+      description: { contains: queryFilters.description, mode: 'insensitive' },
+    }),
+  };
+
+  const {
+    totalGenerics: totalProducts,
+    paginatedGenerics: paginatedProducts,
+    totalPages,
+    hasPrevious,
+    hasNext,
+  } = await getPaginatedData(prisma, prisma.product, where, queryFilters.page);
+
+  return {
+    totalProducts,
+    paginatedProducts,
+    totalPages,
+    hasPrevious,
+    hasNext,
+  };
 };
