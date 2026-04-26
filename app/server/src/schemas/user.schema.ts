1beeb13f4726d8cbed7d1ac36a4350876ab93e85
gabriel.berlucci <gabrielberlucci@proton.me>
2026-04-26 20:00:27 -0300

    feat: add role and confirmation for passwords

--- app/server/src/schemas/user.schema.ts
+++ app/server/src/schemas/user.schema.ts
@@ -15,7 +15,22 @@ export const userSchema = commonSchema
       .max(20, {
         error: 'Senha muito Longa. Deve conter no máximo 20 caracteres',
       }),
-    confirmedPassword: z.string(),
+
+    confirmedPassword: z
+      .string({ error: 'Confirme a senha' })
+      .min(8, {
+        error: 'Senha muito curta. Deve conter pelo menos 8 caracteres',
+      })
+      .max(20, {
+        error: 'Senha muito Longa. Deve conter no máximo 20 caracteres',
+      }),
+
+    role: z.enum(['ADMIN', 'CAIXA']),
+
+    username: z
+      .string({ error: 'Insira um username' })
+      .min(3, { error: 'Username muito curto. Use no minimo 3 caracteres' })
+      .max(8, { error: 'Username muito longo. Use no maximo 8 caracteres' }),
   })
   .refine((data) => data.password === data.confirmedPassword, {
     error: 'As senhas não são iguais',
