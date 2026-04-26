a34464512b0754e9f8bd025224967a47acc4e809
gabriel.berlucci <gabrielberlucci@proton.me>
2026-04-26 20:02:27 -0300

    feat: add controller for user registration

--- app/server/src/controllers/user.controller.ts
+++ app/server/src/controllers/user.controller.ts
@@ -1,3 +1,17 @@
+import { createUser } from '@/services';
 import type { Request, Response } from 'express';
+import { ReasonPhrases, StatusCodes } from 'http-status-codes';
 
-export const registerUserController = async (req: Request, res: Response) => {};
+export const registerUserController = async (req: Request, res: Response) => {
+  const userData = req.body;
+
+  delete userData.confirmedPassword;
+
+  const user = await createUser(userData);
+
+  res.status(StatusCodes.CREATED).send({
+    status: ReasonPhrases.CREATED,
+    message: 'Usuário criado com sucesso',
+    data: user,
+  });
+};
