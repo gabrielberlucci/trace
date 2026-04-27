a9bafa73d5b591cdd463d700d072647d7c739f3c
gabriel.berlucci <gabrielberlucci@proton.me>
2026-04-27 17:33:12 -0300

    feat: index for userLogin Type

--- app/server/src/types/index.ts
+++ app/server/src/types/index.ts
@@ -2,10 +2,12 @@ import { type PaginationQueryParams } from './common.pagination.type';
 import { type CustomerQueryParamsFilters } from './customer.pagination.type';
 import { type SupplierQueryParamsFilters } from './supplier.pagination.type';
 import { type ProductQueryParamsFilters } from './product.pagination.type';
+import { type UserLogin } from './user.login.type';
 
 export type {
   PaginationQueryParams,
   CustomerQueryParamsFilters,
   SupplierQueryParamsFilters,
   ProductQueryParamsFilters,
+  UserLogin,
 };
