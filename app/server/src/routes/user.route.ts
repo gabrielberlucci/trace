4d1a45b8c9adfc718f0424e60e9d5cc15d3d92e1
gabriel.berlucci <gabrielberlucci@proton.me>
2026-04-27 18:53:59 -0300

    feat: add user get routes

--- app/server/src/routes/user.route.ts
+++ app/server/src/routes/user.route.ts
@@ -1,11 +1,16 @@
-import { loginUserController, registerUserController } from '@/controllers';
-import { validateData } from '@/middlewares';
-import { userSchema, userLoginSchema } from '@/schemas';
+import {
+  loginUserController,
+  registerUserController,
+  getUsersController,
+} from '@/controllers';
+import { validateData, validateQuery } from '@/middlewares';
+import { userSchema, userLoginSchema, userQueryFilterSchema } from '@/schemas';
 import { Router } from 'express';
 
 const userRouter: Router = Router();
 
 userRouter.post('/register', validateData(userSchema), registerUserController);
 userRouter.post('/login', validateData(userLoginSchema), loginUserController);
+userRouter.get('/', validateQuery(userQueryFilterSchema), getUsersController);
 
 export { userRouter };
