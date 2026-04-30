274fe7dcd798f159ce4c4c3a4d26f3c98278eb34
gabriel.berlucci <gabrielberlucci@proton.me>
2026-04-30 18:48:08 -0300

    feat: add a condition to check if is passed params, if not get total from totalTables values(trigger)

--- app/server/src/repositories/paginated.repositorhy.ts
+++ app/server/src/repositories/paginated.repositorhy.ts
@@ -15,31 +15,56 @@ export const getPaginatedData = async <T>(
   model: PrismaDelegate<T>,
   where: any,
   page: number,
+  searchTable: Prisma.ModelName,
   omit?: any,
 ) => {
   const PAGE_SIZE = 50;
   const skip = (page - 1) * PAGE_SIZE;
+  let total: number;
+  let data;
 
-  const [totalGenerics, paginatedGenerics] = await tx.$transaction([
-    model.count({
-      where: where,
-    }),
-    model.findMany({
-      where: where,
-      skip: skip,
-      take: PAGE_SIZE,
-      orderBy: { id: 'asc' },
-      omit: omit,
-    }),
-  ]);
+  if (!where || Object.keys(where).length === 0) {
+    const [fastTotal, generics] = await tx.$transaction([
+      tx.totalCount.findUnique({
+        where: { tableName: searchTable },
+      }),
 
-  const totalPages = Math.ceil(totalGenerics / PAGE_SIZE);
+      model.findMany({
+        where: where,
+        skip: skip,
+        take: PAGE_SIZE,
+        orderBy: { id: 'asc' },
+        omit: omit,
+      }),
+    ]);
+
+    total = fastTotal?.total || 0;
+    data = generics;
+  } else {
+    const [slowTotal, filteredGenerics] = await tx.$transaction([
+      model.count({
+        where: where,
+      }),
+      model.findMany({
+        where: where,
+        skip: skip,
+        take: PAGE_SIZE,
+        orderBy: { id: 'asc' },
+        omit: omit,
+      }),
+    ]);
+
+    total = slowTotal;
+    data = filteredGenerics;
+  }
+
+  const totalPages = Math.ceil(total / PAGE_SIZE);
   const hasPrevious: boolean = page > 1;
   const hasNext: boolean = page < totalPages;
 
   return {
-    totalGenerics,
-    paginatedGenerics,
+    total,
+    data,
     totalPages,
     hasPrevious,
     hasNext,
