a67135f2bd1f83f8b9e42f71bc0cf8954abb8191
gabriel.berlucci <gabrielberlucci@proton.me>
2026-04-30 02:32:26 -0300

    feat: addd seed for inserting total counts

--- app/server/prisma/seed.ts
+++ app/server/prisma/seed.ts
@@ -4,6 +4,7 @@ import { PrismaClient } from '../generated/prisma/client';
 import { faker } from '@faker-js/faker';
 import { typePerson } from '../generated/prisma/client';
 import { getCitiesInformation } from '@/clients/cities.clients';
+import { Prisma } from '../generated/prisma/client';
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
@@ -18,9 +19,37 @@ const createRandomCustomer = () => ({
   typePerson: typePerson.PF,
 });
 
+/**
+ * !TODO:
+ * refactor ALL this seed file, because this is probably the worst
+ * piece of code in all this codebase xP
+ */
+
 const customer = faker.helpers.multiple(createRandomCustomer, {
   count: 100,
 });
+
+const tableDatas: Prisma.TotalCountCreateManyInput[] = [
+  {
+    tableName: 'Customer',
+    total: await prisma.customer.count(),
+  },
+
+  {
+    tableName: 'Product',
+    total: await prisma.product.count(),
+  },
+
+  {
+    tableName: 'Supplier',
+    total: await prisma.supplier.count(),
+  },
+
+  {
+    tableName: 'User',
+    total: await prisma.user.count(),
+  },
+];
 
 const seed = async () => {
   // await prisma.customer.createMany({
@@ -30,6 +59,10 @@ const seed = async () => {
   // await prisma.city.createMany({
   //   data: mapped,
   // });
+
+  await prisma.totalCount.createMany({
+    data: tableDatas,
+  });
 };
 
 seed();
