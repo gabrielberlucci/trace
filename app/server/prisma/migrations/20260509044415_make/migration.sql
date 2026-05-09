6c6a1d477b2e5af1cfba2520670565c1554ce7f2
gabriel.berlucci <gabrielberlucci@proton.me>
2026-05-09 01:44:41 -0300

    feat: make fee optional

---
+++ app/server/prisma/migrations/20260509044415_make/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "PaymentMethod" ALTER COLUMN "fee" DROP NOT NULL;
--- app/server/src/schemas/payment.schema.ts
+++ app/server/src/schemas/payment.schema.ts
@@ -2,8 +2,10 @@ import { z } from '@/config/zod.config';
 import { PaymentType } from '../../generated/prisma/client';
 
 export const createPaymentMethodSchema = z.object({
-  description: z.string(),
-  active: z.boolean(),
-  fee: z.float64(),
-  type: z.enum(PaymentType),
+  description: z.string({ error: 'Insira uma descrição' }),
+  active: z.boolean({ error: 'Insira falso ou true' }).optional(),
+  fee: z.float64({ error: 'Insira uma taxa' }).optional(),
+  type: z.enum(PaymentType, {
+    error: 'O tipo deve ser POS, TEF, PIX, CREDITO, DEBITO ou OUTRO',
+  }),
 });
