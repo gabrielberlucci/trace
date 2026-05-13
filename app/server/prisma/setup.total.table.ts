483e79b8a9a8ba279b047816c3fe0e2a92694656
gabriel.berlucci <gabrielberlucci@proton.me>
2026-05-13 01:29:23 -0300

    feat: add a proper setup for total tables count

---
+++ app/server/prisma/setup.total.table.ts
@@ -0,0 +1,49 @@
+import type { Prisma } from '../generated/prisma/client';
+import { prisma } from '../lib/prisma';
+
+interface Table {
+  tablename: string;
+}
+
+interface Total {
+  total: number;
+}
+
+const setupTotalCountTable = async () => {
+  await prisma.$executeRawUnsafe(`TRUNCATE "TotalCount" CASCADE`);
+
+  const tables: Table[] = await prisma.$queryRawUnsafe(
+    `
+    SELECT t.tablename
+    FROM pg_tables t
+            LEFT JOIN "TotalCount" tc ON t.tablename = tc."tableName"
+    WHERE t.schemaname = 'public'
+    AND t.tablename != '_prisma_migrations'
+    AND tc."tableName" IS NULL;
+    `,
+  );
+
+  if (tables.length === 0) {
+    console.log('New tables where not founded');
+    return;
+  }
+
+  const countData: Prisma.TotalCountCreateManyInput[] = await Promise.all(
+    tables.map(async (t) => {
+      const result: Total[] = await prisma.$queryRawUnsafe(`
+            SELECT COUNT(*) as total FROM "${t.tablename}"
+        `);
+
+      return {
+        tableName: t.tablename,
+        total: Number(result.at(0)?.total || 0),
+      };
+    }),
+  );
+
+  await prisma.totalCount.createMany({
+    data: countData,
+  });
+};
+
+setupTotalCountTable();
