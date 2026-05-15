3665f691c5f90e9b6356f01753a5f3493ec79bca
gabriel.berlucci <gabrielberlucci@proton.me>
2026-05-15 02:22:13 -0300

    refactor: move it to a function

--- app/server/prisma/setup.trigger.ts
+++ app/server/prisma/setup.trigger.ts
@@ -4,27 +4,33 @@ interface Table {
   tablename: string;
 }
 
-const tables: Table[] = await prisma.$queryRawUnsafe(
-  `SELECT tablename FROM pg_tables
-    WHERE schemaname = 'public';`,
-);
+const setupTrigger = async () => {
+  const tables: Table[] = await prisma.$queryRawUnsafe(
+    `SELECT tablename
+     FROM pg_tables
+     WHERE schemaname = 'public'
+     AND tablename != '_prisma_migrations';`,
+  );
 
-await Promise.all(
-  tables
-    .filter((table) => table.tablename !== '_prisma_migrations')
-    .map(async (t) => {
-      await prisma.$executeRawUnsafe(
-        `CREATE OR REPLACE TRIGGER trig_a_i_d_count_total
-    AFTER INSERT OR DELETE
-    ON "${t.tablename}"
-    FOR EACH ROW
-EXECUTE FUNCTION count_total_from_table();
+  if (tables.length === 0) return 'No tables founded';
 
-CREATE OR REPLACE TRIGGER trig_a_t_count_total
-    AFTER TRUNCATE
-    ON "${t.tablename}"
-    FOR EACH STATEMENT
-EXECUTE FUNCTION count_total_from_table();`,
-      );
-    }),
-);
+  await Promise.all(
+    tables.map((t) =>
+      prisma.$executeRawUnsafe(`
+        CREATE OR REPLACE TRIGGER trig_a_i_d_count_total
+        AFTER INSERT OR DELETE
+        ON "${t.tablename}"
+        FOR EACH ROW
+        EXECUTE FUNCTION count_total_from_table();
+
+        CREATE OR REPLACE TRIGGER trig_a_t_count_total
+        AFTER TRUNCATE
+        ON "${t.tablename}"
+        FOR EACH STATEMENT
+        EXECUTE FUNCTION count_total_from_table();
+      `),
+    ),
+  );
+};
+
+setupTrigger();
