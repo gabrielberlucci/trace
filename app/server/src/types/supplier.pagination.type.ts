d7e3e8a3848b03fd4ab51762adabbc78a0fb2a9b
gabriel.berlucci <gabrielberlucci@proton.me>
2026-05-16 18:57:29 -0300

    feat: add active:boolean to types that uses it

--- app/server/src/types/supplier.pagination.type.ts
+++ app/server/src/types/supplier.pagination.type.ts
@@ -3,5 +3,5 @@ import type { PaginationQueryParams } from './common.pagination.type';
 export interface SupplierQueryParamsFilters extends PaginationQueryParams {
   document: string;
   name: string;
-  active: string;
+  active: boolean;
 }
--- app/server/src/types/user.pagination.type.ts
+++ app/server/src/types/user.pagination.type.ts
@@ -2,4 +2,5 @@ import type { PaginationQueryParams } from './common.pagination.type';
 
 export interface UserQueryParamsFilters extends PaginationQueryParams {
   username: string;
+  active: boolean;
 }
