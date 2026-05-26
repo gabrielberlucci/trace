a814837aa5838444e1fe3fb1caa06b4f28ede93e
gabriel.berlucci <gabrielberlucci@proton.me>
2026-05-26 00:14:44 -0300

    delete: utils.ts

--- app/web/src/lib/util.ts
+++
@@ -1,7 +0,0 @@
-// From ShadCN
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
