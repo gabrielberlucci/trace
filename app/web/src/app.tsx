e503e52d39ae85acbb4b3a7a8fc2d0aacb8a7ef0
gabriel.berlucci <gabrielberlucci@proton.me>
2026-05-29 01:12:53 -0300

    feat: add routerprovider in App render

--- app/web/src/app.tsx
+++ app/web/src/app.tsx
@@ -1,5 +1,16 @@
+import { createRouter, RouterProvider } from '@tanstack/react-router';
+import { routeTree } from './routeTree.gen';
+
+const router = createRouter({ routeTree });
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
+
 function App() {
-  return <h1>Hello</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export { App };
