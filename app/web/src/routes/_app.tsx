9a9a35635412e1d0292ed4101d2740b3ebb4dc60
gabriel.berlucci <gabrielberlucci@proton.me>
2026-06-01 21:00:27 -0300

    feat: add new main routes to provide sidebar and theme provider and header

---
+++ app/web/src/routes/_app.tsx
@@ -0,0 +1,22 @@
+import { createFileRoute, Outlet } from '@tanstack/react-router';
+import { AppHeader } from '@/components/app-header';
+import { AppSidebar } from '@/components/app-sidebar';
+import { SidebarProvider } from '@/components/ui/sidebar';
+
+export const Route = createFileRoute('/_app')({
+  component: AppLayout,
+});
+
+function AppLayout() {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <div className="flex w-full flex-col bg-muted/20 min-h-screen">
+        <AppHeader />
+        <main className="flex-1 overflow-auto p-4 lg:p-8">
+          <Outlet />
+        </main>
+      </div>
+    </SidebarProvider>
+  );
+}
