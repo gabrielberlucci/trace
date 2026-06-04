6aab7b6f72dd53adf9eb69673ff2028f83844bab
gabriel.berlucci <gabrielberlucci@proton.me>
2026-06-04 17:21:45 -0300

    feat: check if is logged, if not redirect to login

--- app/web/src/api/api.client.ts
+++ app/web/src/api/api.client.ts
@@ -9,3 +9,16 @@ export const apiClient = axios.create({
   },
   withCredentials: true,
 });
+
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('is_logged_in');
+      if (window.location.pathname !== '/login') {
+        window.location.href = '/login';
+      }
+    }
+    return Promise.reject(error);
+  }
+);
