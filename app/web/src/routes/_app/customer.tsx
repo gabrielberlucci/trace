6a59fb515541fc3be54b09ffbdca377b49e497cf
gabriel.berlucci <gabrielberlucci@proton.me>
2026-06-09 02:50:55 -0300

    feat: add ?page for URL

--- app/web/src/routes/_app/customer.tsx
+++ app/web/src/routes/_app/customer.tsx
@@ -1,7 +1,16 @@
 import CustomerPage from '@/pages/customers';
 import { createFileRoute } from '@tanstack/react-router';
 
+type CustomerSearch = {
+  page: number;
+}
+
 export const Route = createFileRoute('/_app/customer')({
+  validateSearch: (search: Record<string, unknown>): CustomerSearch => {
+    return {
+      page: Number(search?.page ?? 1),
+    };
+  },
   component: RouteComponent,
 });
 
