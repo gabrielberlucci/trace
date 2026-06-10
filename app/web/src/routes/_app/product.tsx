4b06b1f80a1e67667e779c99ff99f356b34aef9a
gabriel.berlucci <gabrielberlucci@proton.me>
2026-06-10 03:23:59 -0300

    feat: add route page param

--- app/web/src/routes/_app/product.tsx
+++ app/web/src/routes/_app/product.tsx
@@ -1,7 +1,16 @@
 import ProductsPage from '@/pages/products';
 import { createFileRoute } from '@tanstack/react-router';
 
+type ProductSearch = {
+  page: number;
+}
+
 export const Route = createFileRoute('/_app/product')({
+  validateSearch: (search: Record<string, unknown>): ProductSearch => {
+    return {
+      page: Number(search?.page ?? 1),
+    };
+  },
   component: RouteComponent,
 });
 
--- app/web/src/routes/_app/supplier.tsx
+++ app/web/src/routes/_app/supplier.tsx
@@ -1,7 +1,16 @@
 import SuppliersPage from '@/pages/suppliers';
 import { createFileRoute } from '@tanstack/react-router';
 
+type SupplierSearch = {
+  page: number;
+}
+
 export const Route = createFileRoute('/_app/supplier')({
+  validateSearch: (search: Record<string, unknown>): SupplierSearch => {
+    return {
+      page: Number(search?.page ?? 1),
+    };
+  },
   component: RouteComponent,
 });
 
