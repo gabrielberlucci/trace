c64f90e90e09016fddf3f934f37d6eda67f1a3e5
gabriel.berlucci <gabrielberlucci@proton.me>
2026-06-10 03:11:48 -0300

    feat: add default page = 1

--- app/web/src/api/products/get-product.ts
+++ app/web/src/api/products/get-product.ts
@@ -1,7 +1,9 @@
-import type { PaginatedProducts } from '@/types';
+import type { PaginatedProducts } from '@/types/product-type';
 import { apiClient } from '../api.client';
 
-export const getProducts = async (page: number): Promise<PaginatedProducts> => {
+export const getProducts = async (
+  page: number = 1,
+): Promise<PaginatedProducts> => {
   const result = await apiClient.get<PaginatedProducts>(
     `/products?page=${page}`,
   );
