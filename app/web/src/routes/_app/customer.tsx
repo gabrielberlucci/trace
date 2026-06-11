fd1bc6e45ba87ace19aa1254b63edca11d30474e
gabriel.berlucci <gabrielberlucci@proton.me>
2026-06-11 18:21:43 -0300

    feat: add search for query and active

--- app/web/src/routes/_app/customer.tsx
+++ app/web/src/routes/_app/customer.tsx
@@ -3,12 +3,16 @@ import { createFileRoute } from '@tanstack/react-router';
 
 type CustomerSearch = {
   page: number;
+  q?: string;
+  active?: string;
 }
 
 export const Route = createFileRoute('/_app/customer')({
   validateSearch: (search: Record<string, unknown>): CustomerSearch => {
     return {
       page: Number(search?.page ?? 1),
+      q: typeof search?.q === 'string' ? search.q : undefined,
+      active: typeof search?.active === 'string' ? search.active : undefined,
     };
   },
   component: RouteComponent,
--- app/web/src/routes/_app/supplier.tsx
+++ app/web/src/routes/_app/supplier.tsx
@@ -3,12 +3,16 @@ import { createFileRoute } from '@tanstack/react-router';
 
 type SupplierSearch = {
   page: number;
+  q?: string;
+  active?: string;
 }
 
 export const Route = createFileRoute('/_app/supplier')({
   validateSearch: (search: Record<string, unknown>): SupplierSearch => {
     return {
       page: Number(search?.page ?? 1),
+      q: typeof search?.q === 'string' ? search.q : undefined,
+      active: typeof search?.active === 'string' ? search.active : undefined,
     };
   },
   component: RouteComponent,
