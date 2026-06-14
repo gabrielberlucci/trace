b88b5c971b053fa6fa885f6c83af21bd619719e5
gabriel.berlucci <gabrielberlucci@proton.me>
2026-06-14 01:17:32 -0300

    feat: add validation to / route to see if user is logged in

--- app/web/src/routes/index.tsx
+++ app/web/src/routes/index.tsx
@@ -1,9 +1,16 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
+  beforeLoad: ({ context }) => {
+    if (context.auth.isAuthenticated) {
+      throw redirect({ to: '/dashboard' })
+    } else {
+      throw redirect({ to: '/login' })
+    }
+  },
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/"!</div>
+  return null
 }
