81d645ff952d0360090860fa151432747e7f114d
gabriel.berlucci <gabrielberlucci@proton.me>
2026-06-18 03:46:59 -0300

    deleted: remove cnpj/cpf validation to use on share

--- app/server/src/utils/index.ts
+++ app/server/src/utils/index.ts
@@ -1,12 +1,4 @@
 import { formatPrismaError } from './format.prisma.error';
-import { validateCpf } from './validate.cpf';
-import { validateCnpj } from './validate.cnpj';
 import { hashPassword, verifyPassword } from './password.utils';
 
-export {
-  formatPrismaError,
-  validateCpf,
-  validateCnpj,
-  hashPassword,
-  verifyPassword,
-};
+export { formatPrismaError, hashPassword, verifyPassword };
