ef51f40f3a4734bf7558ebf70d14829cb11c0f10
gabriel.berlucci <gabrielberlucci@proton.me>
2026-06-21 02:21:23 -0300

    feat: add localization router

--- app/server/src/routes/localization.route.ts
+++ app/server/src/routes/localization.route.ts
@@ -1,5 +1,9 @@
+import { getStatesController } from "@/controllers";
+import { authMiddleware } from "@/middlewares";
 import { Router } from "express";
 
 const localizationRouter: Router = Router();
+
+localizationRouter.get("/state", authMiddleware, getStatesController);
 
 export { localizationRouter };
