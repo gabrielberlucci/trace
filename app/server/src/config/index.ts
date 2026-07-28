19f02c92186f7225968115dcaafa5febad9665fe
gabriel.berlucci <gabrielberlucci@proton.me>
2026-07-28 03:19:30 -0300

    feat: initial multer config

--- app/server/src/config/index.ts
+++ app/server/src/config/index.ts
@@ -2,3 +2,4 @@ import { swaggerConfigOptions } from './swagger.config';
 import { corsOptions } from './cors';
 
 export { swaggerConfigOptions, corsOptions };
+export { upload } from './multer';
---
+++ app/server/src/config/multer.ts
@@ -0,0 +1,32 @@
+import { BadRequest } from '@/error';
+import multer from 'multer';
+import path from 'path';
+
+const uploadDir = path.join(process.cwd(), 'uploads', 'pending');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadDir);
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '_' + Math.round(Math.random() * 1e9);
+    const ext = path.extname(file.originalname);
+    cb(
+      null,
+      `${path.basename(file.originalname, '.xml')}_${file.fieldname}_${uniqueSuffix}${ext}`,
+    );
+  },
+});
+
+export const upload = multer({
+  storage: storage,
+  fileFilter: (_req, file, cb) => {
+    if (!file.originalname.startsWith('NFe_'))
+      return cb(new BadRequest('Arquivo XML com nome inválido'));
+    if (file.mimetype === 'text/xml' || file.mimetype === 'application/xml')
+      cb(null, true);
+    else {
+      cb(new BadRequest(`Apenas arquivos XML's podem ser enviados.`));
+    }
+  },
+});
