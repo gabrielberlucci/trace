2f0f573edd0bc2ee73e79db8154ad2092b3bf468
gabriel.berlucci <gabrielberlucci@proton.me>
2026-07-29 03:09:07 -0300

    feat: add await for service to add file

--- app/server/src/controllers/upload.xml.controller.ts
+++ app/server/src/controllers/upload.xml.controller.ts
@@ -1,11 +1,13 @@
 import { BadRequest } from '@/error';
+import { uploadXMLService } from '@/services';
 import type { Request, Response } from 'express';
 import { ReasonPhrases, StatusCodes } from 'http-status-codes';
 
 export const uploadXMLController = async (req: Request, res: Response) => {
   const file = req.file;
-
   if (!file) throw new BadRequest('Nenhum arquivo encontrado para upload.');
+
+  await uploadXMLService(file.destination, file.filename);
 
   res.status(StatusCodes.ACCEPTED).send({
     status: ReasonPhrases.ACCEPTED,
