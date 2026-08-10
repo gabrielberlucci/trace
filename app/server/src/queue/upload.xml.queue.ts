89f6d434d41f91395d21275c1429b4f66b6575eb
gabriel.berlucci <gabrielberlucci@proton.me>
2026-08-10 19:56:47 -0300

    feat: add 5 attemps and 5 min delay

--- app/server/src/queue/upload.xml.queue.ts
+++ app/server/src/queue/upload.xml.queue.ts
@@ -1,5 +1,7 @@
 import { Queue } from 'bullmq';
 
+const delayTime = 5 * 60000;
+
 const importXMLQueue = new Queue('upload-xml', {
   connection: {
     host: process.env.REDIS_HOST,
@@ -11,8 +13,18 @@ export const importXMLQueueService = async (
   xmlPathLocation: string,
   xmlFilename: string,
 ) => {
-  await importXMLQueue.add('upload-xml', {
-    path: xmlPathLocation,
-    name: xmlFilename,
-  });
+  await importXMLQueue.add(
+    'upload-xml',
+    {
+      path: xmlPathLocation,
+      name: xmlFilename,
+    },
+    {
+      attempts: 5,
+      backoff: {
+        type: 'fixed',
+        delay: delayTime,
+      },
+    },
+  );
 };
