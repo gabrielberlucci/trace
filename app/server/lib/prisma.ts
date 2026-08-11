c8db9642c0f59a17bfd9df9cec1c8e6537c4a4d1
gabriel.berlucci <gabrielberlucci@proton.me>
2026-08-11 20:49:06 -0300

    feat: add neon adpter and pg local adapter depending on env mode

--- app/server/lib/prisma.ts
+++ app/server/lib/prisma.ts
@@ -1,10 +1,28 @@
 import 'dotenv/config';
+import { PrismaClient } from '../generated/prisma/client.js';
+import { PrismaNeon } from '@prisma/adapter-neon';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../generated/prisma/client';
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = process.env.DATABASE_URL!;
 
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter, log: ['query', 'error'] });
+const prismaClientSingleton = () => {
+  if (process.env.NODE_ENV === 'development') {
+    const adapter = new PrismaPg({ connectionString });
+    return new PrismaClient({ adapter, log: ['query', 'error'] });
+  } else {
+    const adapter = new PrismaNeon({ connectionString });
+    return new PrismaClient({ adapter });
+  }
+};
+
+declare global {
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prismaGlobal = prisma;
+}
 
 export { prisma };
