bf6422bee75f56b1671aee10c01902d076a242e2
gabriel.berlucci <gabrielberlucci@proton.me>
2026-08-11 22:51:45 -0300

    chore: remove neon adapter

--- app/server/lib/prisma.ts
+++ app/server/lib/prisma.ts
@@ -1,28 +1,10 @@
 import 'dotenv/config';
-import { PrismaClient } from '../generated/prisma/client.js';
-import { PrismaNeon } from '@prisma/adapter-neon';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/prisma/client';
 
-const connectionString = process.env.DATABASE_URL!;
+const connectionString = `${process.env.DATABASE_URL}`;
 
-const prismaClientSingleton = () => {
-  if (process.env.NODE_ENV === 'development') {
-    const adapter = new PrismaPg({ connectionString });
-    return new PrismaClient({ adapter, log: ['query', 'error'] });
-  } else {
-    const adapter = new PrismaNeon({ connectionString });
-    return new PrismaClient({ adapter });
-  }
-};
-
-declare global {
-  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
-}
-
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
-
-if (process.env.NODE_ENV !== 'production') {
-  globalThis.prismaGlobal = prisma;
-}
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter, log: ['query', 'error'] });
 
 export { prisma };
