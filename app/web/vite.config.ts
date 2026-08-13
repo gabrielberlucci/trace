0644ab993adc838f1506a958b4d083d106463f47
gabriel.berlucci <gabrielberlucci@proton.me>
2026-08-13 00:57:57 -0300

    feat: add path for shared

--- app/web/vite.config.ts
+++ app/web/vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@app/shared': path.resolve(__dirname, '../shared/src/index.ts'),
     },
   },
 });
