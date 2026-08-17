dea3bf5381a669d1e4540b31fd2d8ad0a40ba925
gabriel.berlucci <gabrielberlucci@proton.me>
2026-08-17 19:17:20 -0300

    feat: change date to ISO

--- app/shared/src/schemas/service.order.schema.ts
+++ app/shared/src/schemas/service.order.schema.ts
@@ -1,14 +1,14 @@
 import { z } from './config';
 
 export const serviceOrderSchema = z.object({
-  date: z.date({ error: 'Insira a uma data' }),
+  date: z.iso.date({ error: 'Insira a uma data' }),
   document: z
     .string({ error: 'Insira o documento do cliente' })
     .min(11, { error: 'O documento deve conter pelo menos 11 dígitos' })
     .max(14, { error: 'O documento deve conter no máximo 14 dígitos' }),
   items: z.array(
     z.object({
-      data: z.date({ error: 'Insira a uma data' }),
+      data: z.iso.date({ error: 'Insira a uma data' }),
       description: z
         .string({ error: 'Insira uma descrição' })
         .min(1, { error: 'A descrição deve conter pelo menos 1 caracteres' })
