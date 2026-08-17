575c0cf4f2969420af69597aa41ce7912e132754
gabriel.berlucci <gabrielberlucci@proton.me>
2026-08-17 19:25:02 -0300

    feat: add min array to 1 len and erros for date and numbers

--- app/shared/src/schemas/service.order.schema.ts
+++ app/shared/src/schemas/service.order.schema.ts
@@ -6,21 +6,25 @@ export const serviceOrderSchema = z.object({
     .string({ error: 'Insira o documento do cliente' })
     .min(11, { error: 'O documento deve conter pelo menos 11 dígitos' })
     .max(14, { error: 'O documento deve conter no máximo 14 dígitos' }),
-  items: z.array(
-    z.object({
-      data: z.iso.date({ error: 'Insira a uma data' }),
-      description: z
-        .string({ error: 'Insira uma descrição' })
-        .min(1, { error: 'A descrição deve conter pelo menos 1 caracteres' })
-        .max(100, {
-          error: 'A descrição deve conter no máximo 100 caracteres',
-        }),
-      hours: z
-        .number()
-        .check(z.gt(0, { error: 'As horas devem ser maiores que 0' })),
-      hourlyRate: z
-        .number()
-        .check(z.gte(0, { error: 'As horas devem ser igual ou maior que 0' })),
-    }),
-  ),
+  items: z
+    .array(
+      z.object({
+        date: z.iso.date({ error: 'Insira a uma data' }),
+        description: z
+          .string({ error: 'Insira uma descrição' })
+          .min(1, { error: 'A descrição deve conter pelo menos 1 caracteres' })
+          .max(100, {
+            error: 'A descrição deve conter no máximo 100 caracteres',
+          }),
+        hours: z
+          .number({ error: 'Insira a quantia de horas' })
+          .check(z.gt(0, { error: 'As horas devem ser maiores que 0' })),
+        hourlyRate: z
+          .number({ error: 'Insira o valor hora' })
+          .check(
+            z.gte(0, { error: 'As horas devem ser igual ou maior que 0' }),
+          ),
+      }),
+    )
+    .min(1, { error: 'A ordem de serviço deve conter pelo menos 1 item' }),
 });
