8da661be8330ad6ec28fbc41ac1d245eb4e164be
gabriel.berlucci <gabrielberlucci@proton.me>
2026-08-24 23:16:56 -0300

    refactor: function name

--- app/server/src/controllers/dashboard.controller.ts
+++ app/server/src/controllers/dashboard.controller.ts
@@ -1,4 +1,4 @@
-import { getAverageTicket } from '@/services';
+import { dashboardService } from '@/services';
 import type { Request, Response } from 'express';
 import { StatusCodes, ReasonPhrases } from 'http-status-codes';
 
@@ -6,7 +6,7 @@ export const dashboardController = async (req: Request, res: Response) => {
   const startDate: string = res.locals.query.startDate;
   const endDate: string = res.locals.query.endDate;
 
-  const data = await getAverageTicket(startDate, endDate);
+  const data = await dashboardService(startDate, endDate);
 
   res.status(StatusCodes.OK).send({
     status: ReasonPhrases.OK,
