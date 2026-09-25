c466a00cc9ba4664b266efc22c36af482856b945
gabriel.berlucci <gabrielberlucci@proton.me>
2026-09-25 18:51:02 -0300

    feat: create post route

--- app/server/src/routes/stock.adjustment.route.ts
+++ app/server/src/routes/stock.adjustment.route.ts
@@ -1,5 +1,15 @@
+import { authMiddleware, validateData } from '@/middlewares';
 import { Router } from 'express';
+import { stockAdjustmentSchema } from '@app/shared';
+import { createStockAdjustmentController } from '@/controllers';
 
 const stockAdjustmentRouter: Router = Router();
+
+stockAdjustmentRouter.post(
+  '/',
+  authMiddleware,
+  validateData(stockAdjustmentSchema),
+  createStockAdjustmentController,
+);
 
 export { stockAdjustmentRouter };
